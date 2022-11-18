bf956d8f5fdd346d69fe2a72aaea0a5bace1eec9
kubacienciala <jakubciencialaa@gmail.com>
2022-11-18 21:19:49 +0100

    Books mapped

--- App.tsx
+++ App.tsx
@@ -1,9 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+
+const API_KEY = "antrim::stepzen.net+1000::0b3d5e6cff59c140626b0f0e80f3d5b3381628cbac2dc7af3c19abc936aaf4de";
+
+const client = new ApolloClient({
+  uri: "https://antrim.stepzen.net/api/garish-mongoose/__graphql",
+  headers: {
+    Authorization: `Apikey ${API_KEY}`,
+  },
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -14,7 +25,9 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
+        <ApolloProvider client={client}>
         <Navigation colorScheme={colorScheme} />
+        </ApolloProvider>
         <StatusBar />
       </SafeAreaProvider>
     );
